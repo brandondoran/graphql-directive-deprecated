427f3606304276ce3696e6ca9341a94040205587
Brandon Doran <bdoran@gmail.com>
2018-05-08 21:36:02 -0700

    Initial commit

---
+++ src/deprecated-directive.ts
@@ -0,0 +1,18 @@
+import { GraphQLEnumValue, GraphQLField } from 'graphql';
+import { SchemaDirectiveVisitor } from 'graphql-tools';
+
+export class DeprecatedDirective extends SchemaDirectiveVisitor {
+  public constructor(config: any) {
+    super(config);
+  }
+
+  public visitFieldDefinition(field: GraphQLField<any, any>) {
+    field.isDeprecated = true;
+    field.deprecationReason = this.args.reason;
+  }
+
+  public visitEnumValue(value: GraphQLEnumValue) {
+    value.isDeprecated = true;
+    value.deprecationReason = this.args.reason;
+  }
+}
